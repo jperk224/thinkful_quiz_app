77d7894cb54d23a79b1af084382356614458e56f
Jeff <jmperkinson@gmail.com>
2019-05-25 22:53:49 -0400

    functions to render the score and the form using JS

--- index.js
+++ index.js
@@ -12,6 +12,27 @@ function createRandomQuestionIndex(arr) {
     return indexValue;
 }
 
+function generateScoreString(int, arr) {
+    return `
+        <h2>${int}/${arr.length}</h2>
+    `
+}
+
+function genarateFormString() {
+    console.log('creating form');
+    return `
+    <form class="quiz_start">
+        <fieldset>
+            <legend>Atlanta Braves Quiz App</legend>
+            <p>Welcome to the Quiz!</p>
+            <p>Score as many points as you can and win a prize!</p>
+            <button class="js-start">Start</button>
+            <button class="js-quit">Quit</button>
+        </fieldset>
+    </form>
+    `
+}
+
 function generateQuestionString(arr, index) {
     // return the element from 'questions' for the index given
     console.log('creating question');
@@ -25,6 +46,15 @@ function generateQuestionString(arr, index) {
             <div class="d answer"><p>${arr[index].answerD}</p></div>
         </div>
     `
+}
+
+function quizLoop(arr1, arr2) {
+    // loop through remaining questionIndex with event listeners to answer each question
+    while(arr.length > 0) {
+        var index = createRandomQuestionIndex(arr);
+        renderQuestion(questions, index);
+        // TODO: Add quiz logic and score keeping
+    }
 }
 
 //////////Event Listeners//////////
@@ -41,23 +71,34 @@ function startGame(arr1, arr2) {
 }
 
 //////////View//////////
+function renderScore(int, arr) {
+    const score = generateScoreString(int, arr);
+    console.log("rendering score");
+    $(".score").html(score);
+}
+
+function renderForm() {
+    const form = genarateFormString();
+    console.log('rendering form');
+    $(".container").html(form);
+}
+
 function renderQuestion(arr, index) {
-    // render the welcome form to start the game or quit
-    console.log('rendering question');
     const question = generateQuestionString(arr, index);
+    console.log('rendering question');
     $(question).insertAfter(".quiz_start");
 }
 
 function quizGame() {
     // callback function when the page loads
+    // render initial score
+    renderScore(0, questions);
     // Welcome the user and allow them to start or quit
+    renderForm();
     // Activate the functions responsible for handling user events (e.g. start, quit, play, playAgain)
     startGame(questionIndexArray, questions);
-    // TODO: loop through remaining questionIndex with event listeners to answer each question
-    // while(questionIndexArray.length > 0) {
-    //     var index = createRandomQuestionIndex(questionIndexArray);
-    //     renderQuestion(questions, index);
-    // }
+    // loop through remaining questionIndex with event listeners to answer each question
+    // quizLoop(questionIndexArray, questions);
 }
 
 // When the page loads, call quizGame
