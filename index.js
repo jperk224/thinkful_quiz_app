2d20b538177e4f1a133892e0bf3a731770589f86
Jeff <jmperkinson@gmail.com>
2019-05-29 13:04:39 -0400

    user can answer first question

--- index.js
+++ index.js
@@ -4,16 +4,16 @@ function createGameArray(questionArray) {
 
 let gameArray = createGameArray(questions);
 let playerScore = 0;
-let questionNumber = 5;
+let questionNumber = 2;
 
 // Create a question
 function createQuestion(gameArray) {
   let questionString = ``;
+  let correctAnswer = ``;
   if (questionNumber < gameArray.length) {
     questionString = `
-        <form class="question">
             <fieldset>
-                <legend>Question X of X</legend> 
+                <legend>Question ${questionNumber} of ${gameArray.length}</legend> 
                 <p>${gameArray[questionNumber].question}</p>
                 <button class="answer">${
                   gameArray[questionNumber].answers[0]
@@ -28,14 +28,33 @@ function createQuestion(gameArray) {
                   gameArray[questionNumber].answers[3]
                 }</button>
             </fieldset>
-        </form>
         `;
-    console.log(questionString);
+    correctAnswer = gameArray[questionNumber].correctAnswer;
   } else {
     questionString = `Game Over`;
   }
-  return questionString;
+  return [questionString, correctAnswer];
 }
+
+// answer question
+function answerQuestion(correctAnswer) {
+    $('.answer').on('click', function(event) {
+        event.preventDefault();
+        const userAnswer = $(this).text();
+        if(userAnswer === correctAnswer) {
+          console.log('correct!');
+        } else {
+          console.log('wrong!');
+        }
+    });
+}
+
+// Render the player's score
+// Parameters -- player's score, question index pointer array
+function renderScore(score, gameArray) {
+    const scoreString = `<h2>Score: ${score}/${gameArray.length}</h2>`;
+    $(".score").html(scoreString);
+  }
 
 function startGame(indexArray, quesitonsArr) {
   $(".js_game_start").on("click", function(event) {
@@ -48,12 +67,17 @@ function startGame(indexArray, quesitonsArr) {
 }
 
 function renderQuestion(gameArray) {
-  $(".question_container").html(createQuestion(gameArray));
+  let questionArray = createQuestion(gameArray);
+  $(".question").html(questionArray[0]);
+  return questionArray[1];
 }
 
 function playGame() {
-  startGame();
-  renderQuestion(gameArray);
+    renderScore(playerScore, gameArray);
+    let questionAnswer = renderQuestion(gameArray);
+    startGame();
+    answerQuestion(questionAnswer);
+    // renderNextQuestion();
 }
 
 $(playGame);
