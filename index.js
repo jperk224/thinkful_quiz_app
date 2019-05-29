6035948ba620f2134b60d4feaddc350d4a38e872
Jeff <jmperkinson@gmail.com>
2019-05-29 16:31:58 -0400

    answering quesiton renders wrong/right form based on answer

--- index.js
+++ index.js
@@ -10,10 +10,14 @@ let questionNumber = 2;
 function createQuestion(gameArray) {
   let questionString = ``;
   let correctAnswer = ``;
+  let correctForm = ``;
+  let wrongForm = ``;
   if (questionNumber < gameArray.length) {
     questionString = `
             <fieldset>
-                <legend>Question ${questionNumber} of ${gameArray.length}</legend> 
+                <legend>Question ${questionNumber} of ${
+      gameArray.length
+    }</legend> 
                 <p>${gameArray[questionNumber].question}</p>
                 <button class="answer">${
                   gameArray[questionNumber].answers[0]
@@ -30,33 +34,50 @@ function createQuestion(gameArray) {
             </fieldset>
         `;
     correctAnswer = gameArray[questionNumber].correctAnswer;
+    correctForm = `
+            <fieldset>
+                <p>You got it! ${correctAnswer} is correct!</p>
+                <button class="continue">Continue</button>
+            </fieldset>
+    `;
+    wrongForm = `
+    <fieldset>
+                <p>Wrong! The correct answer is ${correctAnswer}</p>
+                <button class="continue">Continue</button>
+            </fieldset>
+    `;
   } else {
     questionString = `Game Over`;
   }
-  return [questionString, correctAnswer];
+  return [questionString, correctAnswer, correctForm, wrongForm];
 }
 
 // answer question
 function answerQuestion(correctAnswer) {
-    $('.answer').on('click', function(event) {
-        event.preventDefault();
-        const userAnswer = $(this).text();
-        if(userAnswer === correctAnswer) {
-          console.log('correct!');
-          playerScore ++;
-          renderScore(playerScore, gameArray);
-        } else {
-          console.log('wrong!');
-        }
-    });
+  $(".answer").on("click", function(event) {
+    event.preventDefault();
+    const userAnswer = $(this).text();
+    if (userAnswer === correctAnswer) {
+      playerScore++;
+      renderScore(playerScore, gameArray);
+      renderCorrectForm(gameArray)
+    } else {
+      renderWrongForm(gameArray);
+    }
+  });
+}
+
+// Continue to next quesiton from feedback div
+function nextQuestion() {
+  // event listener to close feedback div and render next question
 }
 
 // Render the player's score
 // Parameters -- player's score, question index pointer array
 function renderScore(score, gameArray) {
-    const scoreString = `<h2>Score: ${score}/${gameArray.length}</h2>`;
-    $(".score").html(scoreString);
-  }
+  const scoreString = `<h2>Score: ${score}/${gameArray.length}</h2>`;
+  $(".score").html(scoreString);
+}
 
 function startGame(indexArray, quesitonsArr) {
   $(".js_game_start").on("click", function(event) {
@@ -69,17 +90,27 @@ function startGame(indexArray, quesitonsArr) {
 }
 
 function renderQuestion(gameArray) {
-  let questionArray = createQuestion(gameArray);
-  $(".question").html(questionArray[0]);
-  return questionArray[1];
+  let singleQuestionArray = createQuestion(gameArray);
+  $(".question").html(singleQuestionArray[0]);
+  return singleQuestionArray[1];
+}
+
+function renderCorrectForm(gameArray) {
+  let singleQuestionArray = createQuestion(gameArray);
+  $(".question").html(singleQuestionArray[2]);
+}
+
+function renderWrongForm(gameArray) {
+  let singleQuestionArray = createQuestion(gameArray);
+  $(".question").html(singleQuestionArray[3]);
 }
 
 function playGame() {
-    renderScore(playerScore, gameArray);
-    let questionAnswer = renderQuestion(gameArray);
-    startGame();
-    answerQuestion(questionAnswer);
-    // renderNextQuestion();
+  renderScore(playerScore, gameArray);
+  let questionAnswer = renderQuestion(gameArray);
+  startGame();
+  answerQuestion(questionAnswer);
+  // renderNextQuestion();
 }
 
 $(playGame);
