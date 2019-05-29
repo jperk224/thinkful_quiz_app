44a768bafffa727ce7fa2b2da9ff3b4b65c71245
Jeff <jmperkinson@gmail.com>
2019-05-29 08:37:18 -0400

    basic html, css, and datastore stood up

---
+++ datastore.js
@@ -0,0 +1,152 @@
+let questions = [
+    {
+        id: 1,
+        question: 'Who holds the record for the most homeruns in Atlanta Braves franchise history?',
+        answers: [
+            'Hank Aaron', 
+            'Chipper Jones', 
+            'Dale Murphy', 
+            'Eddie Mathews'
+        ],
+        correctAnswer: 'Hank Aaron',
+        imgAttr: {
+            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
+            alt: 'Hank Aaron'
+        }
+    },
+    {
+        id: 2,
+        question: 'Who is the youngest player to hit a homerun in the World Series?',
+        answers: [
+            'Mark Lemke - 18 years, 204 days', 
+            'Andruw Jones - 19 years, 180 days',
+            'Chipper Jones - 21 years, 187 days',
+            'Jason Heyward - 19 years, 82 days'
+        ],
+        correctAnswer: 'Andruw Jones - 19 years, 180 days',
+        imgAttr: {
+            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
+            alt: 'Hank Aaron'
+        }
+    },
+    {
+        id: 3,
+        question: 'What is the name of the current Braves Mascot?',
+        answers: [
+            'Chief Noc-A-Homa',
+            'Philly Phanatic',
+            'San Diego Chicken',
+            'Blooper'
+        ],
+        correctAnswer: 'Blooper',
+        imgAttr: {
+            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
+            alt: 'Hank Aaron'
+        }
+    },
+    {
+        id: 4,
+        question: 'What was Chipper Jones\' final career OBP?',
+        answers: [
+            '.392',
+            '.578',
+            '.401',
+            '.312'
+        ],
+        correctAnswer: '.401',
+        imgAttr: {
+            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
+            alt: 'Hank Aaron'
+        }
+    },
+    {
+        id: 5,
+        question: 'How many Cy Young awards did Greg Maddux win while pitching in Atlanta?',
+        answers: [
+            'Three',
+            'Four',
+            'Two',
+            'Six'
+        ],
+        correctAnswer: 'Three',
+        imgAttr: {
+            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
+            alt: 'Hank Aaron'
+        }
+    },
+    {
+        id: 6,
+        question: 'Which of these pitchers was not a member of the \'Big Three\' Braves pitching rotation?',
+        answers: [
+            'Greg Maddux',
+            'John Smoltz',
+            'Tom Glavine',
+            'Kent Mercker'
+        ],
+        correctAnswer: 'Kent Mercker',
+        imgAttr: {
+            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
+            alt: 'Hank Aaron'
+        }
+    },
+    {
+        id: 7,
+        question: 'The Atlanta Braves defeated the Cleveland Indians to with the 1995 World Series.  Who caught the ceremonial first pitch to open the series in Atlanta?',
+        answers: [
+            'Mike Mordecai',
+            'Chipper Jones',
+            'Javy Lopez',
+            'Damon Berryhill'
+        ],
+        correctAnswer: 'Mike Mordecai',
+        imgAttr: {
+            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
+            alt: 'Hank Aaron'
+        }
+    },
+    {
+        id: 8,
+        question: 'Which of these players spent their entire career wearing an Atlanta Braves uniform?',
+        answers: [
+            'Chipper Jones',
+            'Javy Lopex',
+            'Greg Maddux',
+            'Tom Glavine'
+        ],
+        correctAnswer: 'Chipper Jones',
+        imgAttr: {
+            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
+            alt: 'Hank Aaron'
+        }
+    },
+    {
+        id: 9,
+        question: 'In 2005, Brian McCann hit his first career postseason home run in the NLDS against the Houston Astros.  Whom did he hit it off of?',
+        answers: [
+            'Jeff Bagwell',
+            'Doug Drabek',
+            'Roger Clemens',
+            'Andy Pettitte'
+        ],
+        correctAnswer: 'Roger Clemens',
+        imgAttr: {
+            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
+            alt: 'Hank Aaron'
+        }
+    },
+    {
+        id: 10,
+        question: 'When was the last time the Atlanta Braves won a playoff series?',
+        answers: [
+            '2012',
+            '2007',
+            '2001',
+            '1998'
+        ],
+        correctAnswer: '2001',
+        imgAttr: {
+            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
+            alt: 'Hank Aaron'
+        }
+    }
+]
