268950a2b49c6a2fec8b74c293d6928160088a3a
Jeff <jmperkinson@gmail.com>
2019-05-30 07:57:59 -0400

    added image urls to quesiton objects

--- datastore.js
+++ datastore.js
@@ -11,7 +11,7 @@ let questions = [
         correctAnswer: 'Hank Aaron',
         imgAttr: {
             url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
-            alt: 'Hank Aaron'
+            alt: 'A picture of Hank Aaron'
         }
     },
     {
@@ -25,128 +25,128 @@ let questions = [
         ],
         correctAnswer: 'Andruw Jones - 19 years, 180 days',
         imgAttr: {
-            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
-            alt: 'Hank Aaron'
+            url: 'https://bloximages.newyork1.vip.townnews.com/postandcourier.com/content/tncms/assets/v3/editorial/b/e3/be3919bc-076e-11e8-85ad-534b9e9749c7/5a0b0bbf3fb1f.image.jpg?resize=1200%2C1010',
+            alt: 'A picture of Andruw Jones'
         }
     },
-    {
-        id: 3,
-        question: 'What is the name of the current Braves Mascot?',
-        answers: [
-            'Chief Noc-A-Homa',
-            'Philly Phanatic',
-            'San Diego Chicken',
-            'Blooper'
-        ],
-        correctAnswer: 'Blooper',
-        imgAttr: {
-            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
-            alt: 'Hank Aaron'
-        }
-    },
-    {
-        id: 4,
-        question: 'What was Chipper Jones\' final career OBP?',
-        answers: [
-            '.392',
-            '.578',
-            '.401',
-            '.312'
-        ],
-        correctAnswer: '.401',
-        imgAttr: {
-            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
-            alt: 'Hank Aaron'
-        }
-    },
-    {
-        id: 5,
-        question: 'How many Cy Young awards did Greg Maddux win while pitching in Atlanta?',
-        answers: [
-            'Three',
-            'Four',
-            'Two',
-            'Six'
-        ],
-        correctAnswer: 'Three',
-        imgAttr: {
-            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
-            alt: 'Hank Aaron'
-        }
-    },
-    {
-        id: 6,
-        question: 'Which of these pitchers was not a member of the \'Big Three\' Braves pitching rotation?',
-        answers: [
-            'Greg Maddux',
-            'John Smoltz',
-            'Tom Glavine',
-            'Kent Mercker'
-        ],
-        correctAnswer: 'Kent Mercker',
-        imgAttr: {
-            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
-            alt: 'Hank Aaron'
-        }
-    },
-    {
-        id: 7,
-        question: 'The Atlanta Braves defeated the Cleveland Indians to with the 1995 World Series.  Who caught the ceremonial first pitch to open the series in Atlanta?',
-        answers: [
-            'Mike Mordecai',
-            'Chipper Jones',
-            'Javy Lopez',
-            'Damon Berryhill'
-        ],
-        correctAnswer: 'Mike Mordecai',
-        imgAttr: {
-            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
-            alt: 'Hank Aaron'
-        }
-    },
-    {
-        id: 8,
-        question: 'Which of these players spent their entire career wearing an Atlanta Braves uniform?',
-        answers: [
-            'Chipper Jones',
-            'Javy Lopex',
-            'Greg Maddux',
-            'Tom Glavine'
-        ],
-        correctAnswer: 'Chipper Jones',
-        imgAttr: {
-            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
-            alt: 'Hank Aaron'
-        }
-    },
-    {
-        id: 9,
-        question: 'In 2005, Brian McCann hit his first career postseason home run in the NLDS against the Houston Astros.  Whom did he hit it off of?',
-        answers: [
-            'Jeff Bagwell',
-            'Doug Drabek',
-            'Roger Clemens',
-            'Andy Pettitte'
-        ],
-        correctAnswer: 'Roger Clemens',
-        imgAttr: {
-            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
-            alt: 'Hank Aaron'
-        }
-    },
-    {
-        id: 10,
-        question: 'When was the last time the Atlanta Braves won a playoff series?',
-        answers: [
-            '2012',
-            '2007',
-            '2001',
-            '1998'
-        ],
-        correctAnswer: '2001',
-        imgAttr: {
-            url: 'https://sabr.org/sites/default/files/images/AaronHenry1.jpg',
-            alt: 'Hank Aaron'
-        }
-    }
+    // {
+    //     id: 3,
+    //     question: 'What is the name of the current Braves Mascot?',
+    //     answers: [
+    //         'Chief Noc-A-Homa',
+    //         'Philly Phanatic',
+    //         'San Diego Chicken',
+    //         'Blooper'
+    //     ],
+    //     correctAnswer: 'Blooper',
+    //     imgAttr: {
+    //         url: 'https://bloximages.newyork1.vip.townnews.com/mdjonline.com/content/tncms/assets/v3/editorial/d/09/d0961e6c-03b2-11e8-9f33-7b1181d96291/5a6cff6d54763.image.jpg?resize=1200%2C800',
+    //         alt: 'A picture of Blooper'
+    //     }
+    // },
+    // {
+    //     id: 4,
+    //     question: 'What was Chipper Jones\' final career OBP?',
+    //     answers: [
+    //         '.392',
+    //         '.578',
+    //         '.401',
+    //         '.312'
+    //     ],
+    //     correctAnswer: '.401',
+    //     imgAttr: {
+    //         url: 'https://myareanetwork-photos.s3.amazonaws.com/editorphotos/f/29284_1532961704.png',
+    //         alt: 'A picture of Chipper Jones'
+    //     }
+    // },
+    // {
+    //     id: 5,
+    //     question: 'How many Cy Young awards did Greg Maddux win while pitching in Atlanta?',
+    //     answers: [
+    //         'Three',
+    //         'Four',
+    //         'Two',
+    //         'Six'
+    //     ],
+    //     correctAnswer: 'Three',
+    //     imgAttr: {
+    //         url: 'https://i.pinimg.com/originals/10/08/60/1008600da4ad990743d8411d1df6485d.jpg',
+    //         alt: 'A picture of Greg Maddux'
+    //     }
+    // },
+    // {
+    //     id: 6,
+    //     question: 'Which of these pitchers was not a member of the \'Big Three\' Braves pitching rotation?',
+    //     answers: [
+    //         'Greg Maddux',
+    //         'John Smoltz',
+    //         'Tom Glavine',
+    //         'Kent Mercker'
+    //     ],
+    //     correctAnswer: 'Kent Mercker',
+    //     imgAttr: {
+    //         url: 'http://media.bizj.us/view/img/1721821/greg-maddux-john-smoltz-tom-glavine-3*400xx1972-1972-0-0.jpg',
+    //         alt: 'A picture of the Braves Big Three'
+    //     }
+    // },
+    // {
+    //     id: 7,
+    //     question: 'The Atlanta Braves defeated the Cleveland Indians to with the 1995 World Series.  Who caught the ceremonial first pitch to open the series in Atlanta?',
+    //     answers: [
+    //         'Mike Mordecai',
+    //         'Chipper Jones',
+    //         'Javy Lopez',
+    //         'Damon Berryhill'
+    //     ],
+    //     correctAnswer: 'Mike Mordecai',
+    //     imgAttr: {
+    //         url: 'https://kronozio.blob.core.windows.net/images/card/bb381ef8a0e2446f9f82014cbb955055_front.jpg',
+    //         alt: 'A picture of Mike Mordecai'
+    //     }
+    // },
+    // {
+    //     id: 8,
+    //     question: 'Which of these players spent their entire career wearing an Atlanta Braves uniform?',
+    //     answers: [
+    //         'Chipper Jones',
+    //         'Javy Lopex',
+    //         'Greg Maddux',
+    //         'Tom Glavine'
+    //     ],
+    //     correctAnswer: 'Chipper Jones',
+    //     imgAttr: {
+    //         url: 'https://cdn.theathletic.com/app/uploads/2018/07/28123933/GettyImages-910305530-1024x683.jpg',
+    //         alt: 'A picture of Chipper Jones'
+    //     }
+    // },
+    // {
+    //     id: 9,
+    //     question: 'In 2005, Brian McCann hit his first career postseason home run in the NLDS against the Houston Astros.  Whom did he hit it off of?',
+    //     answers: [
+    //         'Jeff Bagwell',
+    //         'Doug Drabek',
+    //         'Roger Clemens',
+    //         'Andy Pettitte'
+    //     ],
+    //     correctAnswer: 'Roger Clemens',
+    //     imgAttr: {
+    //         url: 'https://zackchum.files.wordpress.com/2010/12/brian-mccann.jpg',
+    //         alt: 'A picture of Brian McCann'
+    //     }
+    // },
+    // {
+    //     id: 10,
+    //     question: 'When was the last time the Atlanta Braves won a playoff series?',
+    //     answers: [
+    //         '2012',
+    //         '2007',
+    //         '2001',
+    //         '1998'
+    //     ],
+    //     correctAnswer: '2001',
+    //     imgAttr: {
+    //         url: 'https://i.pinimg.com/originals/d0/6f/fc/d06ffc8c044d98c154071cfbe66815a5.jpg',
+    //         alt: 'A picture of the 2001 Atlanta Braves second baseman Marcus Giles'
+    //     }
+    // }
 ]
